c8b262f7ee15d6ee55abb5c378797834cf4c7d0d
DOMINIC MAABOBRA TUOLONG <tuolongdominic@gmail.com>
2024-05-13 19:15:13 +0000

    Updated github workflow

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    distDir: "build",
-    basePath: "",
-    sassOptions: {
-        includePaths: ['./src/styles'],
-      },
+  distDir: "build",
+  output: 'export',
+  assetPrefix: process.env.ASSET_PREFIX,
+  basePath: process.env.BASE_PATH,
+
+  sassOptions: {
+    includePaths: ['./src/styles'],
+  },
 };
 
 export default nextConfig;
